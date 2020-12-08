d94e2a80a5310a0b6ddefa5c29effcf2412be9d8
milovanovic <barun@eecs.berkeley.edu>
2020-12-08 23:20:19 +0100

    Add SPDX-License-Identifier: Apache-2.0

--- src/test/scala/BitReversePingPongSpec.scala
+++ src/test/scala/BitReversePingPongSpec.scala
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package fft
 
 import chisel3.experimental.FixedPoint
--- src/test/scala/SDFFFTRunTimeTester.scala
+++ src/test/scala/SDFFFTRunTimeTester.scala
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package fft
 
 import dsptools.DspTester
